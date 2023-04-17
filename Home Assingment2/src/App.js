950287bd28d86e541d0e59991a0c637cabbf1e8f
DONELSINHA01 <100290944+DONELSINHA01@users.noreply.github.com>
2023-04-17 10:24:09 +0530

    Add files via upload

---
+++ Home Assingment2/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Input from './Components/Input';
+import Output from './Components/Output';
+
+
+function App() {
+
+    return(
+      <div className='App'>
+      <Output/>
+      <Input/>
+      </div>
+    );
+    
+}
+
+export default App;
